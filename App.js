1a1b9f9bacf4f5d025cba0d417c6e1107cc1ed1d
banao <tatitachi870@gmail.com>
2021-08-03 17:22:34 +0900

    駐車場マップを作成

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
@@ -17,7 +17,151 @@ export default function App() {
         </View>
       </View>
       <View style={styles.parkingMap}>
-        <Text>マップ</Text>
+        <View style={styles.mapRow}>
+        {/* 1行目 */}
+          <Text style={styles.mapButton} onPress={() => {console.log('aiueo')}}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+        {/* 2行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 3行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 4行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 5行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 6行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 7行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButton}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 8行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 9行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 10行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 11行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 12行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+        </View>
+        <View style={styles.mapRow}>
+          {/* 13行目 */}
+          <Text style={styles.mapButton}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+          <Text style={styles.mapButtonHidden}>01</Text>
+        </View>
+
+        
       </View>
       <View style={styles.parkingInformation}>
         <Text style={{fontSize: 20}}>駐車場所：2F 35</Text>
@@ -46,10 +190,31 @@ const styles = StyleSheet.create({
   parkingMap: {
     flex: 5,
   },
+  mapRow: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'whitesmoke'
+  },
+  mapButton: {
+    flex: 1,
+    backgroundColor: "lightblue",
+    padding: 10,
+    margin: 1,
+    borderWidth: 1,
+    borderColor: "lightblue",
+  },
+  mapButtonHidden: {
+    flex: 1,
+    padding: 10,
+    margin: 1,
+    borderRadius: 20,
+    borderWidth: 1,
+    opacity: 0
+  },
   parkingInformation: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   }
   
 });
